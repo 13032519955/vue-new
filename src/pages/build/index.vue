7d2df56eea8666300728840d9b26df66eff7e8aa
davych <davych@163.com>
2018-11-27 22:33:43 +0800

    -

---
+++ src/pages/build/index.vue
@@ -0,0 +1,23 @@
+<template>
+      <div>
+          index
+      </div>
+</template>
+<script>
+export default {
+  name: 'build',
+  data: function() {
+        return {
+        }
+  },
+  computed: {
+  },
+  mounted: function() {
+    
+  },
+  components:{
+  }
+}
+</script>
+<style>
+</style>
