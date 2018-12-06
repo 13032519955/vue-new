950a4b95333aba8652d0463e483233497bf0309f
davych <davych@163.com>
2018-12-06 17:19:14 +0800

    -

--- src/router/page/build.js
+++ src/router/page/build.js
@@ -1,18 +1,4 @@
-
-import LoadingComponent from '@/frame/loadingComponent'
-import ErrorComponent from '@/frame/ErrorComponent'
-import bb from '@/pages/build'
-//  todo
-const Build = () => ({
-  // component: import(),
-  component: new Promise((resolve, reject) => {
-    setTimeout(() => resolve(bb), 30000)
-  }),
-  loading: LoadingComponent,
-  error: ErrorComponent,
-  delay: 200,
-  timeout: 3000
-})
+const Build = () => import(/* webpackChunkName: "group-build" */ '@/pages/build')
 export default [
   // --------------build---------------
   {
